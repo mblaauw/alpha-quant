184a6306cc4f419cc484e46cc5080246675a9fb5
Michel Blaauw <michel.blaauw@gmail.com>
2026-06-11 09:11:30 +0200

    P1.4: SEC ticker map connector

--- alpha_quant/adapters/real/__init__.py
+++ alpha_quant/adapters/real/__init__.py
@@ -3,12 +3,14 @@ from alpha_quant.adapters.real.base_connector import BaseConnector
 from alpha_quant.adapters.real.clock import SystemClock
 from alpha_quant.adapters.real.eodhd_connector import EODHDConnector
 from alpha_quant.adapters.real.event_sink import SqliteEventSink
+from alpha_quant.adapters.real.sec_connector import SECConnector
 from alpha_quant.adapters.real.token_bucket import TokenBucket
 
 __all__ = [
     "AlpacaConnector",
     "BaseConnector",
     "EODHDConnector",
+    "SECConnector",
     "SqliteEventSink",
     "SystemClock",
     "TokenBucket",
